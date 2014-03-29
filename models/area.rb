7fdb477b4fe48ec2e9ad5c4e97e4ad906525a2f1
Ryan Liang <ryanliang@gmail.com>
2014-03-29 16:50:11 -0400

    use before_create hook to set up default values

--- models/area.rb
+++ models/area.rb
@@ -1,3 +1,8 @@
 class Area < Sequel::Model
   plugin :timestamps
+
+  def before_create
+    self.display_name = self.name
+    super
+  end
 end 
--- models/execution.rb
+++ models/execution.rb
@@ -1,3 +1,26 @@
 class Execution < Sequel::Model
   plugin :timestamps
+
+  def before_create
+    self.created_at ||= Time.now
+    self.updated_at ||= self.created_at
+
+    stats = parse_stats(self.raw_stats)
+
+    self.duration = stats[0]
+    self.total    = stats[1]
+    self.failed   = stats[2]
+
+    super
+  end
+
+  private
+
+  def parse_stats(data)
+    stats    = data.split(',')
+    duration = stats[0].delete('seconds').strip.to_f
+    total    = stats[1].delete('examples').strip.to_i
+    failed   = stats[2].delete('failures').strip.to_i
+    [duration, total, failed]
+  end
 end
