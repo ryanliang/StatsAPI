60af47e4b88f611de771569aa206ee589917f50c
Ryan Liang <ryanliang@gmail.com>
2014-03-30 07:50:03 -0400

    remove test file

--- db/test.rb
+++
@@ -1,9 +0,0 @@
-require 'sequel'
-
-DB = Sequel.connect("sqlite://#{Dir.pwd}/dev.db")
-
-areas = DB[:areas]
-
-# areas.each { |r| r.update(:display_name => 'abc' ) }
-areas.update(display_name: :name)
-areas.each { |r| p r}
