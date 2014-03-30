c5c94479e6ed0acb92062338db0d741d95ab09e2
Ryan Liang <ryanliang@gmail.com>
2014-03-30 09:02:44 -0400

    move post data handling to a separate class

---
+++ helpers/stats_processor.rb
@@ -0,0 +1,34 @@
+class StatsProcessor
+
+  def self.create(stats)
+    suite, name, size = stats['test_suite'], stats['area'], stats['size']
+    area = Area.first(test_suite: suite, name: name, size: size)
+
+    if area.nil?
+      Area.new do |a|
+        a.test_suite = suite
+        a.name = name
+        a.size = size
+        a.save
+      end
+      area = Area.first(test_suite: suite, name: name, size: size)
+    end
+
+    Execution.new do |e|
+      e.area_id = area.area_id
+      e.year = stats['year']
+      e.release = stats['release']
+      e.raw_stats = stats['raw_stats']
+      e.tc_agent = stats['tc_agent']
+      e.env = stats['env']
+      e.install = stats['install']
+      e.os = stats['os']
+      e.browser = stats['browser']
+      e.major_version = stats['major_version']
+      e.minor_version = stats['minor_version']
+      e.save
+    end
+    '{"msg":"successful"}'
+  end
+
+end
--- stats_api.rb
+++ stats_api.rb
@@ -7,10 +7,10 @@ DB = Sequel.connect("sqlite://#{Dir.pwd}/db/dev.db")
 
 require_relative './models/execution'
 require_relative './models/area'
+require_relative './helpers/stats_processor'
 
 get '/' do
-  'Hello world!'
-  @title = 'ryan liang'
+  @executions = Execution.all
   erb :index
 end
 
@@ -20,32 +20,5 @@ end
 
 post '/stats' do
   @post_data = JSON.parse(request.env["rack.input"].read)
-
-  name, size = @post_data['area'], @post_data['size']
-  area = Area.first(name: name, size: size)
-
-  if area.nil?
-    Area.new do |a|
-      a.name = name
-      a.size = size
-      a.save
-    end
-    area = Area.first(name: name, size: size)
-  end
-
-  Execution.new do |e|
-    e.area_id = area.area_id
-    e.year = @post_data['year']
-    e.release = @post_data['release']
-    e.raw_stats = @post_data['raw_stats']
-    e.tc_agent = @post_data['tc_agent']
-    e.env = @post_data['env']
-    e.install = @post_data['install']
-    e.os = @post_data['os']
-    e.browser = @post_data['browser']
-    e.major_version = @post_data['major_version']
-    e.minor_version = @post_data['minor_version']
-    e.save
-  end
-  'successful'
+  StatsProcessor.create(@post_data)
 end
